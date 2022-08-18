68358985f46b2a8108b07748d42f8e8a8b7038b9
Maratib Ali Khan <maratib@gmail.com>
2022-08-19 03:29:31 +0500

    New vercel test 1

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PostDto, SETTINGS } from './utils/dto';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('all')
+  getAll(): string {
+    return "All data";
+  }
+
+  // @Post()
+  // create1(@Body() dto: PostDto): string {
+  //   return "Hello " + dto.name;
+  // }
+
+  @Post()
+  create(@Body(SETTINGS.VALIDATION_PIPE) dto: PostDto): string {
+    return "Hello " + dto.name;
   }
 }
