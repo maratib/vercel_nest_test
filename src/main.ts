828c372060489ac97fd7c00645fee5ce5d6b0f87
Maratib Ali Khan <maratib@gmail.com>
2022-08-19 04:27:47 +0500

    back to zero

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './v1/app.module';
+import { AppModule } from 'app.module';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
