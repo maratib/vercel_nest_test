f990c9bbed665cd2cf0bedc4efe21970e38b8ce8
Maratib Ali Khan <maratib@gmail.com>
2022-08-19 05:22:42 +0500

    public folder added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
+import helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json, urlencoded } from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Add helmet
+  app.use(helmet());
+
+  app.enableCors();
+
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+  // Set view engine
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   app.setGlobalPrefix('api/v1');
   await app.listen(3000);
 }
